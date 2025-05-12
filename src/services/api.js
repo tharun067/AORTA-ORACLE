0b0968a2fbee40341df2d75e8137715617edbe38
tharun067 <tharunchava126@gmail.com>
2025-05-12 20:40:10 +0530

    API updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Base URL for the ML model API endpoint
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://aorta-oracle.onrender.com";
 
 // Create axios instance
 const apiClient = axios.create({
